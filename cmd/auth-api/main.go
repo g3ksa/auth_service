32ed9d9352f68f49ba03e0cd70a0d15b0c72156d
Artem <temka.kuzmin14@gmail.com>
2023-04-06 13:13:01 +0000

    add sign in logic

--- cmd/auth-api/main.go
+++ cmd/auth-api/main.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"AuthService/config"
+	"fmt"
+	"github.com/gin-gonic/gin"
 	"log"
 )
 
@@ -15,5 +17,19 @@ func init() {
 }
 
 func main() {
+	router := gin.Default()
 
+	router.GET("/cookie", func(c *gin.Context) {
+
+		cookie, err := c.Cookie("gin_cookie")
+
+		if err != nil {
+			cookie = "NotSet"
+			c.SetCookie("gin_cookie", "test", 3600, "/", "localhost", false, true)
+		}
+
+		fmt.Printf("Cookie value: %s \n", cookie)
+	})
+
+	router.Run()
 }
